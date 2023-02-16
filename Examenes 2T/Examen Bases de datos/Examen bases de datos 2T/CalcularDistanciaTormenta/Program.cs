5d22615dc2acf8d26a48bb84f62ced5c4c8d9ea8
inesdeveloper <efreya_sanchez@hotmail.com>
2023-02-16 17:36:21 +0100

    Examen Bases 2T
    Examen de bases de datos, ejercicio tormenta y Tiempo de destino

---
+++ Examenes 2T/Examen Bases de datos/Examen bases de datos 2T/CalcularDistanciaTormenta/Program.cs
@@ -0,0 +1,12 @@
+﻿// Mostramos el mensaje al usuario
+Console.WriteLine("Introduzca el numero de segundos que han pasado:");
+// Leemos los segundos introducidos por consola
+string segundos = Console.ReadLine();
+// La consola devuelve strings, lo pasamos a entero para operar
+int numerosegundos = Int32.Parse(segundos);
+// Creamos una variable con la velocidad del sonido
+int velocidadsonido = 343;
+// Calculamos la distancia
+int distancia = numerosegundos * velocidadsonido;
+// Mostramos el resultado por consola
+Console.WriteLine("La distancia es: " + distancia + " metros");
---
+++ Examenes 2T/Examen Bases de datos/Examen bases de datos 2T/TiempoDelDestino/TiempoDelDestino/Program.cs
@@ -0,0 +1,19 @@
+﻿// Mostramos el mensaje al usuario
+Console.WriteLine("Introduzca la velocidad del coche:");
+// Leemos la velocidad(en km/h) introducida por consola
+string velocidad = Console.ReadLine();
+// Mostramos el mensaje al usuario
+Console.WriteLine("Introduzca la distancia:");
+// Leemos la distancia(en km) introducida por consola
+string distancia = Console.ReadLine();
+
+// La consola devuelve strings, lo pasamos a entero para operar
+int velocidadnumero = Int32.Parse(velocidad);
+// La consola devuelve strings, lo pasamos a entero para operar
+int distancianumero = Int32.Parse(distancia);
+
+// Calculamos el tiempo que se tarda
+float tiempo = (float)distancianumero / velocidadnumero;
+// Mostramos el resultado por consola
+Console.WriteLine("El tiempo que se tarda es: " + tiempo + " hora/s");
+
