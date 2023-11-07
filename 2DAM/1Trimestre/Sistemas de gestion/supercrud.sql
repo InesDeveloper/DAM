c35136d5892eacb1fac917add143c6d89b6da916
inesdeveloper <efreya_sanchez@hotmail.com>
2023-11-07 19:43:47 +0100

    Sistemas de gestion Practica 6
    Cambios en la base de datos (contenido con sentencias y supercontrolador. Cambios para evitar entradas no deseadas

--- 2DAM/1Trimestre/Sistemas de gestion/supercrud.sql
+++ 2DAM/1Trimestre/Sistemas de gestion/supercrud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 27-10-2023 a las 10:09:03
+-- Tiempo de generación: 07-11-2023 a las 18:40:51
 -- Versión del servidor: 5.7.39
 -- Versión de PHP: 7.4.33
 
@@ -28,20 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `entregas` (
-  `Identificador` int(20) NOT NULL,
-  `NIF` varchar(9) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `url` varchar(30) COLLATE utf8mb4_spanish_ci DEFAULT NULL COMMENT 'En este campo debes introducir la url de youtube',
-  `practica` varchar(20) COLLATE utf8mb4_spanish_ci DEFAULT NULL COMMENT 'A continuación debes introducir la práctica que estás entregando',
-  `epoch` int(12) DEFAULT NULL COMMENT 'En este campo aparecerá la marca de tiempo'
+  `Identificador` int(20) NOT NULL COMMENT '{"titulo":"Introduce tu email","descripcion":"Introduce el email que sueles utilizar","placeholder":"ejem:persona@dominio.com","mascara":"algo","min":"5","deshabilitado":"false","visible":"false","tipodato":"number"}',
+  `email` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL COMMENT '{"titulo":"Introduce tu email","descripcion":"Introduce el email que sueles utilizar","placeholder":"ejem:persona@dominio.com","mascara":"algo","min":"5","deshabilitado":"false","visible":"true","icono":"fas fa-at","tipodato":"email"}',
+  `url` varchar(120) COLLATE utf8mb4_spanish_ci NOT NULL COMMENT '{"titulo":"URL del video","descripcion":"En este campo introduce la URL que has copiado y pegado al realizar la entrega del video","placeholder":"ejem:http://youtube.com?v=abc","mascara":"algo","min":"10","deshabilitado":"false","visible":"true","icono":"fab fa-youtube","tipodato":"URL"}',
+  `practica` int(20) NOT NULL COMMENT '{"titulo":"Número de la práctica","descripcion":"Introduce el número de la práctica que vas a subir en esta entrega","placeholder":"ejem: Introduce un número","mascara":"algo","min":"1","deshabilitado":"false","visible":"true","icono":"fas fa-sort-numeric-up","tipodato":"number"}',
+  `epoch` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '{"titulo":"Número de la práctica","descripcion":"Introduce el número de la práctica que vas a subir en esta entrega","placeholder":"ejem: Introduce un número","mascara":"algo","min":"1","deshabilitado":"false","visible":"false","tipodato":"text"}'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `entregas`
 --
 
-INSERT INTO `entregas` (`Identificador`, `NIF`, `url`, `practica`, `epoch`) VALUES
-(1, '12345678A', 'ggadgsagsda', 'fgdsgfhgf', 52353447),
-(2, '87654321B', 'sadsfsdff', 'sdfsdff', NULL);
+INSERT INTO `entregas` (`Identificador`, `email`, `url`, `practica`, `epoch`) VALUES
+(5, 'ines@hotmail.com', 'asdsadasd', 2, '2023-11-02 12:01:21'),
+(7, 'ines2@hotmail.com', 'kdoapajru', 3, '2023-11-04 17:12:02'),
+(8, '1ejemplo@algo.com', 'https://youtu.be/yDp', 1, '2023-11-07 15:49:14'),
+(9, 'efreya_sanchez@hotmail.com', 'https://youtudndkowow', 3, '2023-11-07 18:17:49');
 
 --
 -- Índices para tablas volcadas
@@ -61,7 +63,7 @@ ALTER TABLE `entregas`
 -- AUTO_INCREMENT de la tabla `entregas`
 --
 ALTER TABLE `entregas`
-  MODIFY `Identificador` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Identificador` int(20) NOT NULL AUTO_INCREMENT COMMENT '{"titulo":"Introduce tu email","descripcion":"Introduce el email que sueles utilizar","placeholder":"ejem:persona@dominio.com","mascara":"algo","min":"5","deshabilitado":"false","visible":"false","tipodato":"number"}', AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- 2DAM/1Trimestre/Sistemas de gestion/supercrud/controlador.php
+++ 2DAM/1Trimestre/Sistemas de gestion/supercrud/controlador.php
@@ -9,33 +9,33 @@ class Supercontrolador {
         $consulta = "SHOW FULL COLUMNS FROM ".$tabla;
         $resultado = $mysqli->query($consulta);
         while ($fila = $resultado->fetch_assoc()) {
-            if($fila["Key"] == NULL && $fila["Field"] != "epoch"){
+            if(json_decode($fila["Comment"])->visible == "true"){
+                preg_match('#\((.*?)\)#', $fila["Type"], $match);
                 echo '
                     <div class="campo">
-                        
-                        <p>'.$fila["Comment"].'</p>
+                        <h3>'.json_decode($fila["Comment"])->titulo.'</h3>
+                        <p>'.json_decode($fila["Comment"])->descripcion.' - Caracteres mínimo '.json_decode($fila["Comment"])->min.' máximo '.$match[1].'</p>
                         ';
                         if($fila["Null"] == "NO"){echo " <p>* Campo requerido</p>";}
+                        
                         echo'
-                        <input type="';
-                        if($fila["Field"] == "email"){
-                            echo "email";
-                        }
-                        else if(strpos($fila["Type"],"varchar") !== false){
-                            echo "text";
-                        }
-                        else if(strpos($fila["Type"],"int") !== false){
-                            echo "number";
-                        }
-                        echo '" name="'.$fila["Field"].'" id="'.$fila["Field"].'"
+                        <div class="contienecampo">
+                        <table><tr><td width="80%">
+                        <input type="'.json_decode($fila["Comment"])->tipodato.'" name="'.$fila["Field"].'" id="'.$fila["Field"].'"
                         ';
                             if($fila["Null"] == "NO"){echo " required ";}
-                            if($fila["Field"] == "epoch"){echo " disabled ";}
+                            if(json_decode($fila["Comment"])->deshabilitado == "true"){echo " disabled ";}
                             preg_match('#\((.*?)\)#', $fila["Type"], $match);
                             echo '
                             maxlenght = "'.$match[1].'"
-                            placeholder = "'.$fila["Field"].'"
+                            minlenght = "'.json_decode($fila["Comment"])->min.'"
+                            placeholder = "'.json_decode($fila["Comment"])->placeholder.'"
                         >
+                        </td><td width="20%">
+                        <div class="tipocampo"><i class="'.json_decode($fila["Comment"])->icono.'"></i></div>
+                        </td></tr></table>
+                        </div>
+                        <div class="clearfix"></div>
                     </div>
                 ';
             }
@@ -45,7 +45,25 @@ class Supercontrolador {
     }
     
     function procesaformulario(){
-        //echo "Si estas viendo esto, se está procesando el formulario";
+        //Vamos a analizar qué es lo que viene antes de meterlo
+        foreach($_REQUEST as $nombre_campo => $valor){
+            //echo "el campo es: ".$nombre_campo." y el valor es ".$valor;
+            if(preg_match('~\b(delete|drop|truncate)\b~i',$nombre_campo)){
+                $volcado = implode(",", $_REQUEST).",".$_SERVER['REMOTE_ADDR'].",".$_SERVER['HTTP_USER_AGENT']."\n";
+                $myfile = fopen("volcado.txt", "a");
+                fwrite($myfile, $volcado);
+                die("ejecucion detenida");
+            }
+            
+            if(preg_match('~\b(delete|drop|truncate)\b~i',$valor)){
+                $volcado = implode(",", $_REQUEST).",".$_SERVER['REMOTE_ADDR'].",".$_SERVER['HTTP_USER_AGENT']."\n";
+                $myfile = fopen("volcado.txt", "a");
+                fwrite($myfile, $volcado);
+                die("ejecucion detenida");
+            }
+            
+        }
+        
         include "config.php";
         $listadocolumnas = "";
         $listadovalores = "";
@@ -70,6 +88,7 @@ class Supercontrolador {
         ';
         echo '<meta http-equiv="refresh" content="5; url=?" />
         ';
+        
     }
 }
 ?>
