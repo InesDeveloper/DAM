368c3b87968ecc82551db8058957c85251d4ef4c
inesdeveloper <efreya_sanchez@hotmail.com>
2024-01-22 14:48:14 +0100

    Desarrollo de Interfaces Práctica 14

---
+++ 2DAM/2Trimestre/Desarrollo de Interfaces/Unity/AprendiendoUnity2/Assets/Scripts/colosionEstrella.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colosionEstrella : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // jugador = GameObject.Find("jugador");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // if (collision.gameObject.CompareTag(""))
+        // {
+        //     Destroy(gameObject);
+        // }
+    }
+}
---
+++ 2DAM/2Trimestre/Desarrollo de Interfaces/Unity/AprendiendoUnity2/Assets/Scripts/subeCamara.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class subeCamara : MonoBehaviour
+{
+    public Transform objetoDestino;
+    public float desfaseVertical = 0.0f;
+
+    private Camera mi_camara;
+
+    private void Start()
+    {
+        mi_camara = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if(objetoDestino != null)
+        {
+            Vector3 posicion_camara = mi_camara.transform.position;
+            posicion_camara.y = objetoDestino.position.y + desfaseVertical;
+            mi_camara.transform.position = posicion_camara;
+        }
+    }
+}
