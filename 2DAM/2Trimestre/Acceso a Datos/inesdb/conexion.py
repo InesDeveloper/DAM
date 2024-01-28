0e274f9620aed5fcff8728799c1245313a8d3ea3
InesDeveloper <efreya_sanchez@hotmail.com>
2024-01-28 21:29:30 +0100

    Acceso a datos Practica 14

--- 2DAM/2Trimestre/Acceso a Datos/inesdb/conexion.py
+++ 2DAM/2Trimestre/Acceso a Datos/inesdb/conexion.py
@@ -1,4 +1,4 @@
 from inesdb_conector import Inesdb
 
 Conexion1 = Inesdb("miempresa")
-Conexion1.insert("clientes","cliente9","este es otro contenido de prueba")
+Conexion1.insert("clientes","cliente10","este es otro contenido de prueba 10")
